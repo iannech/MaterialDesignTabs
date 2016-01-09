61f2a7b2aba5b9a2ea9ae7a82d0987154ce34189
iannech <ianoniango94@gmail.com>
2016-01-09 12:28:42 +0300

    Tab icons with text

--- app/src/main/java/com/example/kontrol/MainActivity.java
+++ app/src/main/java/com/example/kontrol/MainActivity.java
@@ -21,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
     private TabLayout tabLayout;
+    private int[] tabIcons = {
+            R.drawable.ic_archive_black_24dp,
+            R.drawable.ic_inbox_black_24dp,
+            R.drawable.ic_mail_black_24dp
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,14 +42,23 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+        setUpTabIcons();
 
+    }
+
+    private void setUpTabIcons(){
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
     }
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new OneFragment(), "ONE");
         adapter.addFragment(new TwoFragment(), "TWO");
-        adapter.addFragment(new ThreeFragment(),"THREE");
+        adapter.addFragment(new ThreeFragment(), "THREE");
+        viewPager.setAdapter(adapter);
+        /*
         adapter.addFragment(new FourFragment(),"FOUR");
         adapter.addFragment(new FiveFragment(),"FIVE");
         adapter.addFragment(new SixFragment(),"SIX");
@@ -51,9 +66,9 @@ public class MainActivity extends AppCompatActivity {
         adapter.addFragment(new EightFragment(),"EIGHT");
         adapter.addFragment(new NineFragment(),"NINE");
         adapter.addFragment(new TenFragment(),"TEN");
-        viewPager.setAdapter(adapter);
+         */
 
-}
+    }
     class ViewPagerAdapter extends FragmentPagerAdapter {
 
         private final List<Fragment> mFragmentList = new ArrayList<>();
